a9ae03b1a6c96e5dc9ae338d2e4ba3a31a5d54b4
achraf karrouche <achrafkarrouche@gmail.com>
2017-06-12 13:01:51 +0200

    implimenting FREAK descriptor & SURF Detector

--- framedetactor.cpp
+++ framedetactor.cpp
@@ -6,9 +6,75 @@ FrameDetactor::FrameDetactor(QWidget *parent) :
     ui(new Ui::FrameDetactor)
 {
     ui->setupUi(this);
+#ifdef HAVE_OPENCV_NONFREE
+    cv::initModule_nonfree();
+#else
+    cout << "Warning: OpenCV is built without nonfree support SIFT, SURF and some other algorithms are not available." << endl;
+#endif
+    QGraphicsDropShadowEffect *sh = new QGraphicsDropShadowEffect();
+    sh->setBlurRadius(8);
+    sh->setOffset(2);
+    sh->setColor(QColor(63, 63, 63, 180));
+    this->setGraphicsEffect(sh);
+    //! ******** Detectors ***********
+    mKeypoint.clear();
+    //! ******** Exctractor **********
+    // (rotation invariance, scale invariance, pattern radius corresponding to SMALLEST_KP_SIZE,
+    // number of octaves, optional vector containing the selected pairs)
+    // FREAK extractor(true, true, 22, 4, std::vector<int>());
+
+    //Extractor(true, true, 22, 4, std::vector<int>());
+}
+
+vector<KeyPoint> FrameDetactor::_SurfDetector(const Mat& frame)
+{
+    Mat tmpFrame;
+    frame.copyTo(this->frame);
+    this->frame.copyTo(tmpFrame);
+
+    vector<KeyPoint> tmpkeyPoint;
+    tmpkeyPoint.clear();
+
+    Detector.hessianThreshold = 380.0;
+    Detector.detect(tmpFrame,tmpkeyPoint);
+
+    // draw keypoint on the Image.
+    drawKeypoints(tmpFrame,tmpkeyPoint,tmpFrame);
+    ui->LB_Display->setPixmap(QPixmap::fromImage(convertOpenCVMatToQtQImage(tmpFrame)));
+    ui->Lb_DisplayMsg->setText(QString::number(tmpkeyPoint.size()));
+    mKeypoint = tmpkeyPoint;
+    return tmpkeyPoint;
+}
+
+Mat FrameDetactor::_FreakDescriptor()
+{
+    Mat tmpDescriptor,tmpFrame;
+    this->frame.copyTo(tmpFrame);
+
+    Descriptor_Extractor.compute(tmpFrame,mKeypoint,tmpDescriptor);
+    ui->Lb_DisplayMsg->setText("Descriptor: H"+QString::number(tmpDescriptor.rows)+":"
+                               ":W"+QString::number(tmpDescriptor.cols)+":"
+                               "type :"+QString::number(tmpDescriptor.type()));
+
+    ui->LB_Display->setPixmap(QPixmap::fromImage(convertOpenCVMatToQtQImage(tmpDescriptor)));
+    return tmpDescriptor;
 }
 
 FrameDetactor::~FrameDetactor()
 {
     delete ui;
+}
+//! *****************  EXTRA FUNCTIONS QT & OPENCV WORKS TOGATHER.*******************************
+QImage FrameDetactor::convertOpenCVMatToQtQImage(cv::Mat mat) {
+    if(mat.channels() == 1) {                                   // if 1 channel (grayscale or black and white) image
+        return QImage((uchar*)mat.data, mat.cols, mat.rows, mat.step, QImage::Format_Indexed8);     // return QImage
+    } else if(mat.channels() == 3) {                            // if 3 channel color image
+        cv::cvtColor(mat, mat, CV_BGR2RGB);                     // flip colors
+        return QImage((uchar*)mat.data, mat.cols, mat.rows, mat.step, QImage::Format_RGB888);       // return QImage
+    } else {
+        mToast = new Toast(this);
+        mToast->setMessage(tr("Mat 'frame' wasn't (1 or 3) Channel."));
+
+    }
+    return QImage();        // return a blank QImage if the above did not work
 }
--- framedetactor.h
+++ framedetactor.h
@@ -1,7 +1,34 @@
 #ifndef FRAMEDETACTOR_H
 #define FRAMEDETACTOR_H
 
+#include <QtCore>
 #include <QFrame>
+#include <QGraphicsDropShadowEffect>
+#include <QFile>
+#include <QTextStream>
+
+// QAnimate Framework
+#include <QPropertyAnimation>
+#include <QSequentialAnimationGroup>
+#include <QParallelAnimationGroup>
+#include <QStateMachine>
+#include <QState>
+#include <QSignalTransition>
+
+// OpenCV Framework
+#include "opencv2/opencv_modules.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/calib3d/calib3d.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/features2d/features2d.hpp"
+#ifdef HAVE_OPENCV_NONFREE
+# include "opencv2/nonfree/nonfree.hpp"
+#endif
+
+#include "toast.h"
+
+
+using namespace cv;
 
 namespace Ui {
 class FrameDetactor;
@@ -15,8 +42,22 @@ public:
     explicit FrameDetactor(QWidget *parent = 0);
     ~FrameDetactor();
 
+public slots:
+    vector<KeyPoint> _SurfDetector(const Mat &frame);
+    QImage convertOpenCVMatToQtQImage(cv::Mat mat);
+    Mat _FreakDescriptor();
 private:
-    Ui::FrameDetactor *ui;
+    Ui::FrameDetactor   *ui;
+
+    SurfFeatureDetector      Detector;
+    FREAK                    Descriptor_Extractor;
+    // GenericDescriptorMatcher Descriptor_Matcher;
+
+    Mat frame;
+    vector<KeyPoint> mKeypoint;
+
+    Toast *mToast;
+
 };
 
 #endif // FRAMEDETACTOR_H
